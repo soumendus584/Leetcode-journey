ddd27e1b871b3ffe2c0ba46e3952afdd9821b90a
SOUMENDU SAMANTA <soumendus584@gmail.com>
2026-09-05 14:05:13 +0530

    Time: 4 ms (24.08%), Space: 263.4 MB (25.61%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,13 +1,10 @@
-#include<vector>
-#include<algorithm>
-using namespace std;
-class Solution{
+class Solution {
 public:
-void rotate(vector<int>&nums,int k){
-int n=nums.size();
-k%=n;
-reverse(nums.begin(),nums.end());
-reverse(nums.begin(),nums.begin()+k);
-reverse(nums.begin()+k,nums.end());
-}
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
+         reverse(nums.begin(),nums.end());
+         reverse(nums.begin(),nums.begin()+k);
+         reverse(nums.begin()+k,nums.end());
+    }
 };
